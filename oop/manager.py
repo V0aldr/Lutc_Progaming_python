17877603bf5b2c846a277b838061db56e7d32f2c
voaldr <vorobiovss@gmail.com>
2024-05-08 09:13:53 +0300

    init person_alternative.py && update manager.py

--- oop/manager.py
+++ oop/manager.py
@@ -2,12 +2,19 @@ from person_start import Person
 
 
 class Manager(Person):
-    def give_raise(self, percent, bonus=0.1):
-        self.pay *= (1.0 + percent + bonus)
 
+    def __init__(self, name, age, pay, job):
+        super().__init__(name, age, pay, 'manager')
+        Person.__init__(self, name, age, pay, 'manager')
+
+    def give_raise(self, percent, bonus=0.1):
+        # self.pay *= (1.0 + percent + bonus)
+        # Person.give_raise(self, percent + bonus)
+        super().give_raise(percent + bonus)
 
 if __name__ == '__main__':
-    tom = Manager(name='Tom Doe', age=50, pay=50000)
+    tom = Manager(name='Tom Doe', age=50, pay=50000, job='manager')
+    tom2 = Manager('Tom Joe', 50, 50000, 'manager')
     print(tom.last_name())
     tom.give_raise(.20)
     print(tom.pay)
@@ -15,3 +22,4 @@ if __name__ == '__main__':
     print(tom.__dict__ )
     tom.job = 'manager'
     print(tom.__dict__)
+    print(tom2.__dict__)
